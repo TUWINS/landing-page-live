16bd01610675ce10dc1bad109d1ed99206df3af0
Geli033 <ehlop297@gmail.com>
2026-02-08 01:27:15 -0600

    Image styles

---
+++ src/components/navbar.tsx
@@ -0,0 +1,24 @@
+
+export const Navbar = () => {
+    return (
+    <div>
+ <nav>
+        <ul className='flex gap-6'>
+            <li>
+                <a className='hover:text-teal-200' href="#">Features</a>
+            </li>
+
+            <li>
+                <a className='hover:text-teal-200' href="#">Team</a>
+            </li>
+
+            <li>
+                <a className='hover:text-teal-200' href="#">Sing In</a>
+            </li>
+        </ul>
+    </nav>
+    </div>
+    )
+}
+
+export default Navbar
