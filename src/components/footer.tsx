b71b322c643e3184b0440280d5e4b22d7902ad09
Geli033 <ehlop297@gmail.com>
2026-02-18 01:59:12 -0600

    footer completed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,48 +1,123 @@
+import type { JSX } from "react";
 import { ContactItem } from "./footer/contact-item"
+import { FooterListItem } from "./footer/footer-list-item"
+import { FacebookIcon } from "./footer/facebook-Icon";
+import { XIcon } from "./footer/x-icon";
+import { InstagramIcon } from "./footer/instagram-icon";
 
-interface ContactInformation{
-    text:string,
-    inconUrl:string;
-    alt:string;
+interface ContactInformation {
+  text: string,
+  inconUrl: string;
+  alt: string;
 }
 
 
-const contactInformation: ContactInformation []=[
+const contactInformation: ContactInformation[] = [
   {
-    text:'lorem-ipsum-dolor-sit-amet-consectetur-adipisicing-elit-veritatis-perferendis-iusto-illo-fugiat-vitae-eum-fuga-quos-iure-alias-numquam-alquid-facilis-quis-vero-consectetur-culpa-ab-molestias-rerum-reiciendis',
-    inconUrl:'/images/icon-location.svg',
-    alt:'location'
+    text: 'lorem-ipsum-dolor-sit-amet-consectetur-adipisicing-elit-veritatis-perferendis-iusto-illo-fugiat-vitae-eum-fuga-quos-iure-alias-numquam-alquid-facilis-quis-vero-consectetur-culpa-ab-molestias-rerum-reiciendis',
+    inconUrl: '/images/icon-location.svg',
+    alt: 'location'
   },
   {
-    text:'+1-543-123-4567',
-    inconUrl:'/images/icon-phone.svg',
-    alt:'icon phone'
+    text: '+1-543-123-4567',
+    inconUrl: '/images/icon-phone.svg',
+    alt: 'icon phone'
   },
   {
-    text:'example@fylo.com',
-    inconUrl:'/images/icon-email.svg',
-    alt:'icon email'
+    text: 'example@fylo.com',
+    inconUrl: '/images/icon-email.svg',
+    alt: 'icon email'
   },
 
-]
+];
+
+const pages: string[] = [
+  'About us',
+  'Jobs',
+  'Press',
+  'Blog'
+
+];
+
+const conditions: string[] = [
+  'Contact',
+  'Terms',
+  'Privacy'
+];
+
+interface Media {
+  text: string;
+  icon: JSX.Element;
+
+}
+
+const medias: Media[] = [
+  {
+    text: 'facebook',
+    icon: <FacebookIcon />,
+  },
+  {
+    text:'X',
+    icon: <XIcon/>
+  },
+  {
+    text:'Insta',
+    icon: <InstagramIcon/>
+  }
+];
+
 export const Footer = () => {
   return (
-    <footer className='pt-[260px] pb-[300px] px-6'> 
-        <img className='mb-10' src="/images/logo.svg" alt="Logo" />
-      
-    <div className='flex flex-col gap-4' >
+    <footer className='pt-[260px] pb-[60px] px-6'>
+      <img className='mb-10' src="/images/logo.svg" alt="Logo" />
+
+      <div className='flex flex-col gap-4' >
 
 
-    
-      {
+
+        {
           contactInformation.map
-          (contact=> (
+            (contact => (
               <ContactItem key={contact.alt} {...contact} />
             ))
-      }
-    </div>
+        }
+      </div>
+
+      <div className='flex flex-col gap-8 mt-16'>
+        <ul>
+          {
+            pages.map((page: string) => (
+              <FooterListItem key={page} text={page} />
+
+            ))
+          }
+
+        </ul>
+        <ul>
+          {
+            conditions.map((condition: string) => (
+              <FooterListItem key={condition} text={condition} />
+            ))
+          }
+        </ul>
+
+<div className='flex justify-center gap-4 '>
+  
+  
+          {medias.map((media: Media) => (
+            <div
+              key={media.text}
+              className='text-white hover:text-teal-200
+              cursor-pointer border rounded-full  size-[30px] p-0.5'>
+  
+              {media.icon}
+  
+            </div>
+          ))}
+</div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 
